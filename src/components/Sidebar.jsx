168efdcddf7c112602ed67050e529cf373432f0f
DaviMoreira27 <davimsantana2706@gmail.com>
2025-07-19 13:17:03 -0300

    feat: added dynamic sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "../global.scss";
+
+export default function Sidebar() {
+  const [collapsed, setCollapsed] = useState(false);
+  const year = new Date().getFullYear();
+  
+  return (
+    <aside className={`sidebar ${collapsed ? "collapsed" : ""}`}>
+      <button className="toggle-button" onClick={() => setCollapsed(!collapsed)}>
+        {collapsed ? "»" : "«"}
+      </button>
+      
+      <div className="sidebar-header">Rats Journey</div>
+      
+      <nav className="sidebar-links">
+        <a href="/about" data-initial="A">
+          <h3>About Me</h3>
+        </a>
+        <a href="/blog" data-initial="B">
+          <h3>Blog</h3>
+        </a>
+        <a href="/projects" data-initial="P">
+          <h3>Projects</h3>
+        </a>
+      </nav>
+      
+      <div className="sidebar-footer">
+        <div className="copyright">
+          <small>&copy; {year} Rats Journey</small>
+        </div>
+        <div className="social-links">
+          <a 
+            href="https://m.webtoo.ls/@astro" 
+            target="_blank" 
+            className="social-link mastodon"
+            title="Mastodon"
+          >
+            <span className="social-icon">🐘</span>
+            <span className="social-label">Mastodon</span>
+          </a>
+          <a 
+            href="https://twitter.com/astrodotbuild" 
+            target="_blank" 
+            className="social-link twitter"
+            title="Twitter"
+          >
+            <span className="social-icon">🐦</span>
+            <span className="social-label">Twitter</span>
+          </a>
+          <a 
+            href="https://github.com/withastro/astro" 
+            target="_blank" 
+            className="social-link github"
+            title="GitHub"
+          >
+            <span className="social-icon">⚡</span>
+            <span className="social-label">GitHub</span>
+          </a>
+        </div>
+      </div>
+    </aside>
+  );
+}
