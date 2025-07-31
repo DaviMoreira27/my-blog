f9c7bc837e321a67425abdf5b08c083f41ea0744
DaviMoreira27 <davimsantana2706@gmail.com>
2025-07-31 00:58:52 -0300

    fix: security issues, passing defining security headers and robots.txt directly

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dogatsu.dev',
+    site: 'https://dogatsu.pages.dev',
     output: 'static',
     integrations: [mdx(), sitemap(), react()]
 });
